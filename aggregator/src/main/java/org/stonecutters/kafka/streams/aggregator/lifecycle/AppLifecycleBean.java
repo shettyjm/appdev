d0905947df188f157a6c5f19b2c6a4c95e32c9c6
jmaira <jmaira@redhat.com>
2021-08-18 07:03:21 +0000

    more cahnges

---
+++ aggregator/src/main/java/org/stonecutters/kafka/streams/aggregator/lifecycle/AppLifecycleBean.java
@@ -0,0 +1,24 @@
+package org.stonecutters.kafka.streams.aggregator.lifecycle;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
+
+import io.quarkus.runtime.ShutdownEvent;
+import io.quarkus.runtime.StartupEvent;
+import io.quarkus.runtime.configuration.ProfileManager;
+import org.jboss.logging.Logger;
+
+@ApplicationScoped
+public class AppLifecycleBean {
+
+    private static final Logger LOGGER = Logger.getLogger("ListenerBean");
+
+    void onStart(@Observes StartupEvent ev) {               
+        LOGGER.info("The application is starting, with profile "+ProfileManager.getActiveProfile());
+    }
+
+    void onStop(@Observes ShutdownEvent ev) {               
+        LOGGER.info("The application is stopping with profile "+ProfileManager.getActiveProfile());
+    }
+
+}
